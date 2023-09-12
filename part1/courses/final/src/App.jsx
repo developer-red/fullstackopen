77a07da2fe810694f605634aa9d12b1b5ea044ef
Adam <developer.red@protonmail.com>
2023-09-11 21:38:36 -0600

    added courses exercise

---
+++ part1/courses/final/src/App.jsx
@@ -0,0 +1,62 @@
+const Header = ({course}) => {
+
+    return (<div>
+        <h1> {course}</h1>
+    </div>)
+}
+
+const Content = function ({props}) {
+    return (
+        < div>
+            <Part part={props[0].name} exercises={props[0].exercises}/>
+            <Part part={props[1].name} exercises={props[1].exercises}/>
+            <Part part={props[2].name} exercises={props[2].exercises}/>
+        </div>
+    )
+}
+
+const Part = function ({part, exercises}) {
+    return (
+        <p> {part} {exercises} </p>
+    )
+}
+
+const Total = function ({props}) {
+    console.log(props)
+    const total = props[0].exercises + props[1].exercises + props[2].exercises
+    console.log(total)
+    return (<div>
+            <p>Number of exercises {total}</p>
+        </div>
+    )
+}
+
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+
+
+    return (
+        <div>
+            <Header  course={course.name}/>
+            <Content props={course.parts}/>
+            <Total   props={course.parts}/>
+        </div>
+    )
+}
+export default App
